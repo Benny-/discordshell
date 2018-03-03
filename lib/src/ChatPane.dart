3f00b5fc67f9c16ec2eb15165b261113341daabf
Torca2001 <Torca2001@gmail.com>
2018-03-03 20:36:50 +1100

    Changed status to classes

--- lib/src/ChatPane.dart
+++ lib/src/ChatPane.dart
@@ -92,8 +92,8 @@ class ChatPane {
     if(e.newMember.guild==selectedChannel.guild){
       DivElement userlistitem = document.querySelector(".users-list");
       HtmlElement avatar = userlistitem.querySelector("[title='"+e.newMember.id+"']");
-      ImageElement picture = avatar.parent.querySelector(".user-list-avatar");
-      picture.id=e.newMember.status;
+      ImageElement picture = avatar.parent.querySelector("img");
+      picture.className=e.newMember.status;
     }
   });
 
@@ -148,7 +148,7 @@ class ChatPane {
 
   adduser(discord.Member user) {
     DocumentFragment userFragment = document.importNode(userTemplate.content, true);
-    ImageElement avatar = userFragment.querySelector(".user-list-avatar");
+    ImageElement avatar = userFragment.querySelector(".offline");
     HtmlElement username = userFragment.querySelector(".user-list-name");
   if(user.avatar == null)
     {
@@ -161,7 +161,12 @@ class ChatPane {
     }
     username.title = user.id;
     username.text = user.username;
-    avatar.id=user.status;
+    if (user.status==null){
+      avatar.className="offline";
+      }
+      else{
+        avatar.className=user.status;
+      }
     
     userslist.append(userFragment);
   }
